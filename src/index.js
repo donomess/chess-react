02ec98b01348176b74b5077b39bbaab077ee203d
Donovan Messer <donovanmess01@gmail.com>
2021-09-30 19:55:40 -0400

    maybe fix

--- src/index.js
+++ src/index.js
@@ -1,44 +1,14 @@
 /* Tutorial from https://betterprogramming.pub/how-to-build-a-chess-board-with-javascript-480ab182739e
 and https://codepen.io/macro6461/pen/xxGzydR */
 
+
 var board = document.getElementById('boardInner');
-var numContainer = document.getElementById('numberContainer');
-var letterContainer = document.getElementById('letterContainer');
-var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-var numbers = ['1', '2', '3', '4', '5', '6', '7', '8']
-var display = 'block'
-
-const renderBoard = (useLabels) =>{
-  
-  if (useLabels){
-    renderLabels() 
-  }
-  
-  var change = false
-  var color = 'white'
-  for (var i = 0; i < 64; ++i){
-    var el = document.createElement('DIV')
-    el.className = 'square'
-    change = i % 8 === 0 || i % 8 === NaN
-    color = change ? color : 
-    color === 'white' ? 'black' : 'white'
-    el.style.backgroundColor = color
-    board.appendChild(el)
-    change = false
-  }
-}
-
 const renderBoard1 = (useLabels) =>{
-  
-  if (useLabels){
-    renderLabels() 
-  }
- 
-  for (var i = 0; i < letters.length; ++i){
+  for (var i = 0; i < 8; ++i){
     var row = document.createElement('DIV')
     row.className = 'row'
     row.style.flexDirection = i % 2 === 0 ? '' : 'row-reverse'; 
-    for (var j = 0; j < letters.length; ++j){
+    for (var j = 0; j < 8; ++j){
       var square = document.createElement('DIV')
       square.className = 'square'
       square.style.backgroundColor = j % 2 === 0 ? 'white' : 'black'
@@ -47,17 +17,3 @@ const renderBoard1 = (useLabels) =>{
     board.appendChild(row)
   }   
  }
-
-renderLabels = () => {
-  letters.forEach((x,i)=>{
-    var el = document.createElement('DIV')
-    var el2 = document.createElement('DIV')
-    el.innerText = x
-    el.className = 'label'
-    el2.innerText = numbers[i]
-    el2.className = 'label'
-    numContainer.appendChild(el2)
-    letterContainer.appendChild(el)
-  })
-}
-renderBoard1()
