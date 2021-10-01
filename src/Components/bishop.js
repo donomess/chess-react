170b317fb6e97c2788d6ad34fae9a2ca4a92c70a
Donovan Messer <donovanmess01@gmail.com>
2021-09-30 23:09:42 -0400

    fixed img

--- src/Components/bishop.js
+++ src/Components/bishop.js
@@ -4,7 +4,7 @@ import wbishop from './src/ChessPieces/wbishop.png';
 
 export default class Bishop extends Piece {
     constructor(player){
-      super(player, (player === 1? "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg" : "https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg"));
+      super(player, (player === 1? bbishop : wbishop));
     }
   
     isMovePossible(src, dest){
