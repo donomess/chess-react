c44df9b09b11df66d3c2cf7ec57d53baf4d73eb4
Donovan Messer <donovanmess01@gmail.com>
2021-09-30 22:34:11 -0400

    updated index js to new tutorial

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
-/* Tutorial from https://betterprogramming.pub/how-to-build-a-chess-board-with-javascript-480ab182739e
-and https://codepen.io/macro6461/pen/xxGzydR */
+/*  tutorial used: https://www.techighness.com/post/develop-two-player-chess-game-with-react-js/ */
 
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-var board = document.getElementById('boardInner');
-const renderBoard1 = (useLabels) =>{
-  for (var i = 0; i < 8; ++i){
-    var row = document.createElement('DIV')
-    row.className = 'row'
-    row.style.flexDirection = i % 2 === 0 ? '' : 'row-reverse'; 
-    for (var j = 0; j < 8; ++j){
-      var square = document.createElement('DIV')
-      square.className = 'square'
-      square.style.backgroundColor = j % 2 === 0 ? 'white' : 'black'
-      row.appendChild(square)
-    }
-    board.appendChild(row)
-  }   
- }
+import './index.css';
+//import Game from './components/game.js'
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
