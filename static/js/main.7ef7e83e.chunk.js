ab85a2fd3f374224a0b564bf938021ec6735f83f
Donovan Messer <donovanmess01@gmail.com>
2021-09-30 23:48:20 -0400

    Updates

--- static/js/main.7ef7e83e.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-chess"]=this["webpackJsonpreact-chess"]||[]).push([[0],[function(e,s){document.getElementById("boardInner")}],[[0,1]]]);
-//# sourceMappingURL=main.7ef7e83e.chunk.js.map
